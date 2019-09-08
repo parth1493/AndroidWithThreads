adb4f048f9eff1f708ef5883bf06c9c9743ad2c5
Parth <parth1493@gmail.com>
2019-09-08 03:38:54 -0400

    Initial commit

---
+++ app/src/main/java/com/parth/threadandroid/DictionaryActivity.java
@@ -0,0 +1,199 @@
+package com.parth.threadandroid;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.parth.threadandroid.adapters.WordsRecyclerAdapter;
+import com.parth.threadandroid.models.Word;
+import com.parth.threadandroid.util.FakeData;
+import com.parth.threadandroid.util.VerticalSpacingItemDecorator;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class DictionaryActivity extends AppCompatActivity implements
+        WordsRecyclerAdapter.OnWordListener,
+        View.OnClickListener,
+        SwipeRefreshLayout.OnRefreshListener
+{
+
+    private static final String TAG = "WordsListActivity";
+
+    //ui components
+    private RecyclerView mRecyclerView;
+    private SwipeRefreshLayout mSwipeRefresh;
+
+    //vars
+    private ArrayList<Word> mWords = new ArrayList<>();
+    private WordsRecyclerAdapter mWordRecyclerAdapter;
+    private FloatingActionButton mFab;
+    private String mSearchQuery = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        restoreInstanceState(savedInstanceState);
+
+        setContentView(R.layout.activity_dictionary);
+        mRecyclerView = findViewById(R.id.recyclerView);
+        mFab = findViewById(R.id.fab);
+        mSwipeRefresh = findViewById(R.id.swipe_refresh);
+
+        mFab.setOnClickListener(this);
+        mSwipeRefresh.setOnRefreshListener(this);
+
+
+        setupRecyclerView();
+    }
+
+
+    private void restoreInstanceState(Bundle savedInstanceState){
+        if(savedInstanceState != null){
+            mWords = savedInstanceState.getParcelableArrayList("words");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putParcelableArrayList("words", mWords);
+        super.onSaveInstanceState(outState);
+    }
+
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: called.");
+        super.onStart();
+        if(mWords.size() == 0){
+            retrieveWords();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: called.");
+    }
+
+
+    private void retrieveWords() {
+        Log.d(TAG, "retrieveWords: called.");
+        mWords.addAll(Arrays.asList(FakeData.words));
+    }
+
+
+    public void deleteWord(Word word) {
+        Log.d(TAG, "deleteWord: called.");
+        mWords.remove(word);
+        mWordRecyclerAdapter.getFilteredWords().remove(word);
+        mWordRecyclerAdapter.notifyDataSetChanged();
+
+    }
+
+
+
+    private void setupRecyclerView(){
+        Log.d(TAG, "setupRecyclerView: called.");
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        VerticalSpacingItemDecorator itemDecorator = new VerticalSpacingItemDecorator(10);
+        mRecyclerView.addItemDecoration(itemDecorator);
+        new ItemTouchHelper(itemTouchHelperCallback).attachToRecyclerView(mRecyclerView);
+        mWordRecyclerAdapter = new WordsRecyclerAdapter(mWords, this);
+        mRecyclerView.setAdapter(mWordRecyclerAdapter);
+    }
+
+    @Override
+    public void onWordClick(int position) {
+        Intent intent = new Intent(this, EditWordActivity.class);
+        intent.putExtra("selected_word", mWords.get(position));
+        startActivity(intent);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.fab:{
+                Intent intent = new Intent(this, EditWordActivity.class);
+                startActivity(intent);
+                break;
+            }
+
+        }
+    }
+
+
+    ItemTouchHelper.SimpleCallback itemTouchHelperCallback = new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.RIGHT) {
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            return false;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+            deleteWord(mWords.get(mWords.indexOf(mWordRecyclerAdapter.getFilteredWords().get(viewHolder.getAdapterPosition()))));
+        }
+    };
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.dictionary_activity_actions, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView =
+                (SearchView) searchItem.getActionView();
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setMaxWidth(Integer.MAX_VALUE);
+
+        // listening to search query text change
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                // filter recycler view when query submitted
+                mSearchQuery = query;
+                mWordRecyclerAdapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // filter recycler view when text is changed
+                mSearchQuery = query;
+                mWordRecyclerAdapter.getFilter().filter(query);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public void onRefresh() {
+        retrieveWords();
+        mSwipeRefresh.setRefreshing(false);
+    }
+
+}
+
+
---
+++ app/src/main/java/com/parth/threadandroid/persistence/WordDao.java
@@ -0,0 +1,32 @@
+package com.parth.threadandroid.persistence;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.parth.threadandroid.models.Word;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+
+    @Query("SELECT * FROM Word")
+    List<Word> getAllWords();
+
+    @Insert
+    long[] insertWords(Word... words);
+
+    @Delete
+    int delete(Word note);
+
+    @Query("UPDATE Word SET title = :title, content = :content, timestamp = :timestamp WHERE uid = :uid")
+    int updateWord(String title, String content, String timestamp, int uid);
+
+    @Query("SELECT * FROM Word LIMIT :row, :numRows ")
+    public List<Word> getSomeWords(int row, int numRows);
+
+    @Query("SELECT COUNT(*) FROM Word")
+    public Integer getNumRows();
+}
